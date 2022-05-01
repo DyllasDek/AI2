802274118208d9abe5041f32bc5fd070913f4f02
DyllasDek <danchik.korneenko@yandex.ru>
2022-05-01 18:48:08 +0300

    init

--- DanilaKorneenko.py
+++ DanilaKorneenko.py
@@ -1,1 +1,8 @@
-print("lol")
+import mido
+from mido import MidiFile
+
+mid = MidiFile('input1.mid')
+for i, track in enumerate(mid.tracks):
+    print('Track {}: {}'.format(i, track.name))
+    for msg in track:
+        print(msg)
